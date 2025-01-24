ed497e2ba58772353782c1f0de1799cc9fe12733
Dmytro Boiko <iamboikodmytro@gmail.com>
2025-01-24 20:55:22 +0200

    mate nodejs

---
+++ index.js
@@ -0,0 +1,4 @@
+console.log('Hello world from the nodeJS');
+
+const myName = 'Dima';
+console.log(myName);
---
+++ mate/lesson_2/app.js
@@ -0,0 +1,5 @@
+// import { add, subtract } from './actions.js';
+
+const { add, subtract } = require('./actions.js');
+
+console.log(add(5, 3), subtract(5, 3));
