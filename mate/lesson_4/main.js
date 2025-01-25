534bd408619692b2f8e5052fa0ad6dc3b27212ce
Dmytro Boiko <iamboikodmytro@gmail.com>
2025-01-25 12:09:30 +0200

    work with modules

---
+++ mate/lesson_4/main.js
@@ -0,0 +1,11 @@
+const a = require('a');
+const b = require('b');
+const c = require('c');
+const d = require('d');
+const e = require('e');
+
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+console.log(e);
