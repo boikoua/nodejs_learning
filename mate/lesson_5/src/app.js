08fbac01ac2c8779c2a96f18f1f50ca401184f75
Dmytro Boiko <iamboikodmytro@gmail.com>
2025-01-27 14:28:37 +0200

    learning librares

--- mate/lesson_5/src/app.js
+++ mate/lesson_5/src/app.js
@@ -1,11 +1,13 @@
 function add(...numbers) {
-  let sum = 0;
+    let sum = 0;
+    const hello = 'hello';
+    console.log(hello);
 
-  for (const n of numbers) {
-    sum += n;
-  }
+    for (const n of numbers) {
+        sum += n;
+    }
 
-  return sum;
+    return sum;
 }
 
 console.log(add(1, 2, 3, 4, 5));
