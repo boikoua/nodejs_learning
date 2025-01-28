2569603befdb5cab579de199000bb5dbfe70cfcf
Dmytro Boiko <iamboikodmytro@gmail.com>
2025-01-28 09:01:06 +0200

    lunch server

--- redGroup/main.js
+++
@@ -1,6 +0,0 @@
-import { DATA } from './server.js';
-
-// const { DATA } = require('./service');
-
-console.log('hello from main.js');
-console.log(DATA.domain);
--- redGroup/server.js
+++ redGroup/server.js
@@ -1,7 +1,20 @@
-export const DATA = {
-  domain: 'https://google.com',
-};
+// Создание сервера, и тест запроса
+import express from 'express';
 
-// module.exports = {
-//   DATA,
-// };
+const app = express();
+
+async function main() {
+  app.use(express.json());
+
+  app.use('/api/twit', (request, response) => {
+    response.status(200).json({
+      message: 'success',
+    });
+  });
+
+  app.listen(4200, () => {
+    console.log('Server is running on port 4200');
+  });
+}
+
+main();
